ba47fa6a25d2cbc8e4a23e880347f27bed7a5c9f
vishnu-tsx <vishnu@codewalnut.com>
2025-09-11 17:32:15 +0000

    feat: update favicon and update app header to include cw logo

--- apps/web/src/components/ui/app-header.tsx
+++ apps/web/src/components/ui/app-header.tsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router";
+
+import { CodeWalnutIcon } from "~/components/icons/codewalnut";
+
 export function AppHeader() {
 	return (
 		<header className="sticky top-0 z-10 border-b bg-background/80 backdrop-blur-lg">
 			<div className="mx-auto flex h-16 max-w-7xl items-center justify-between px-4">
-				<h1 className="font-bold text-xl">CodeWalnut</h1>
+				<Link to="/">
+					<CodeWalnutIcon className="scale-85" />
+				</Link>
 			</div>
 		</header>
 	);
